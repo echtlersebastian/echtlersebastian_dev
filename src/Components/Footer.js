aa7c2d45b778181ffea2cdb71d51c53bfab34eb6
Basti <mail@echtlersebastian.de>
2021-11-18 00:36:39 +0100

    finished most stuff.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,9 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>&copy; Copyright 2021 Sebastian Echtler</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Shoutout to Tim Baker for prividing this awesome Template! Check him out at his <a href="https://github.com/tbakerx">Github</a></li>
            </ul>
 
         </div>
