13b797bce0f6ff13c7e66a5c7449671738c00a07
Basti <mail@echtlersebastian.de>
2021-11-17 21:28:53 +0100

    changed some stuff.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-110570651-1');
+    ReactGA.initialize('G-EPHS12CKQ6');
     ReactGA.pageview(window.location.pathname);
 
   }
